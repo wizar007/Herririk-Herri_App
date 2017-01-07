fdfcd3c0f64c6938fb5f717ce689eece5699f812
Mikel De Prado <mikeldeprado@hotmail.com>
2017-01-07 13:30:51 +0100

    locica activity menu y layout activity Herri

---
+++ Herririk-Herri/app/src/main/java/herririk_herri/tta/intel/ehu/eus/herririk_herri/HerriActivity.java
@@ -0,0 +1,22 @@
+package herririk_herri.tta.intel.ehu.eus.herririk_herri;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HerriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_herri);
+        SharedPreferences prefs = getSharedPreferences("herri", MODE_PRIVATE);
+        String restoredText = prefs.getString("herri", null);
+        Button mButton=(Button)findViewById(R.id.herri_butt_kultur);
+        mButton.setText("Ezagutu \""+restoredText+"\"!");
+        this.setTitle(restoredText);
+
+    }
+}
--- Herririk-Herri/app/src/main/java/herririk_herri/tta/intel/ehu/eus/herririk_herri/MenuActivity.java
+++ Herririk-Herri/app/src/main/java/herririk_herri/tta/intel/ehu/eus/herririk_herri/MenuActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 
@@ -16,16 +17,49 @@ public class MenuActivity extends AppCompatActivity {
     }
     public void selherri(View view)
     {
-        //Intent intent=new Intent(this,HerriActivity.class);
+        Intent intent=new Intent(this,HerriActivity.class);
+        SharedPreferences myprefs= this.getSharedPreferences("herri", MODE_WORLD_READABLE);
+        //this.setTitle("Prueba111");
+
 
         switch (view.getId())
         {
             case (R.id.menu_Bermeo):
-                SharedPreferences myprefs= this.getSharedPreferences("herri", MODE_WORLD_READABLE);
                 myprefs.edit().putString("herri","Bermeo").commit();
+                startActivity(intent);
+
+                break;
+            case (R.id.menu_Gernika):
+                myprefs.edit().putString("herri","Gernika").commit();
+                startActivity(intent);
+
+                break;
+            case (R.id.menu_Lekeitio):
+                myprefs.edit().putString("herri","Lekeitio").commit();
+                startActivity(intent);
+
+                break;
+            case (R.id.menu_Zeberio):
+                myprefs.edit().putString("herri","Zeberio").commit();
+                startActivity(intent);
+
+                break;
+            case (R.id.menu_Ondarrua):
+                myprefs.edit().putString("herri","Ondarrua").commit();
+                startActivity(intent);
+
+                break;
+            case (R.id.menu_Zamudio):
+                myprefs.edit().putString("herri","Zamudio").commit();
+                startActivity(intent);
 
                 break;
 
         }
+    }
+    public void test(View view)
+    {
+        //Intent intent=new Intent(this,TestActivity.class);
+        //starActivity(intent);
     }
 }
