91d70bba4b9c35f41468f5b5302b9ddd20ff6550
Mikel De Prado <mikeldeprado@hotmail.com>
2017-01-07 14:14:36 +0100

    selAriketa Layout añadir icono

---
+++ Herririk-Herri/app/src/main/java/herririk_herri/tta/intel/ehu/eus/herririk_herri/SelKulturaActivity.java
@@ -0,0 +1,13 @@
+package herririk_herri.tta.intel.ehu.eus.herririk_herri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelKulturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sel_kultura);
+    }
+}
