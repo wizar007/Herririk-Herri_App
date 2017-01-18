8ba16da9c7f23a24d3e149c602ebf0aedf37cee4
Mikel De Prado <mikeldeprado@hotmail.com>
2017-01-18 19:58:00 +0100

    Carga de datos desde el servidor desde actividad RecordActivity

--- Herririk-Herri/app/src/main/java/herririk_herri/tta/intel/ehu/eus/herririk_herri/RecordActivity.java
+++ Herririk-Herri/app/src/main/java/herririk_herri/tta/intel/ehu/eus/herririk_herri/RecordActivity.java
@@ -4,9 +4,12 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.List;
 
 import herririk_herri.tta.intel.ehu.eus.herririk_herri.model.Exercise;
 import herririk_herri.tta.intel.ehu.eus.herririk_herri.model.Kultura;
@@ -15,6 +18,10 @@ import herririk_herri.tta.intel.ehu.eus.herririk_herri.model.coms.ProgressTask;
 import herririk_herri.tta.intel.ehu.eus.herririk_herri.model.coms.RestClient;
 
 public class RecordActivity extends AppCompatActivity {
+    private int indice=0;
+    private int size=2;
+    final String URL=null;
+    private List<Exercise> lExercise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +30,14 @@ public class RecordActivity extends AppCompatActivity {
         this.setTitle(R.string.Record_title);
     }
 
-    public void load()
+    public void load(View view)
     {
         final String url=getString(R.string.url_server);
         SharedPreferences myprefs=getSharedPreferences("herri",MODE_WORLD_READABLE);
         final String herri=myprefs.getString("herri",null);
         final String arikmota=myprefs.getString("ariketa",null);
         //llamada a logica para solicitar los datos, petición al servidor
-        View element=findViewById(R.id.kultur_button_init);
+        View element=findViewById(R.id.record_button_init);
         element.setVisibility(View.GONE);
         final int indice=0;
         new ProgressTask<ListExercise>(this)
@@ -39,32 +46,31 @@ public class RecordActivity extends AppCompatActivity {
             @Override
             protected ListExercise work() throws Exception {
                 RestClient rest= new RestClient(url);
-                ListExercise kulturas=new ListExercise();
-                JSONObject jsonObject = rest.getJson(String.format("requestKultura?herri=%s&ariketamota=%s",herri,arikmota ));
-                JSONArray lista=jsonObject.getJSONArray("kultura");
+                ListExercise ejercicios=new ListExercise();
+                JSONObject jsonObject = rest.getJson(String.format("requestAriketa?herri=%s&ariketamota=%s",herri,arikmota ));
+                JSONArray lista=jsonObject.getJSONArray("exercises");
 
                 for(int i =0;i<lista.length();i++){
                     JSONObject temp=lista.getJSONObject(i);
 
 
-                    Exercise objeto=new Kultura(temp.getString("img"),temp.getString("informacion"),temp.getString("kulturaType"));
+                    Exercise objeto=new Exercise(temp.getString("ariketaCode"),temp.getString("audio"),temp.getString("enunciado"),temp.getString("img"),temp.getString("opcion_1"),temp.getString("opcion_2"),temp.getString("opcion_3"),temp.getInt("solucion"),temp.getString("tipo"));
 
-                    kulturas.addChoice(objeto);
+                   ejercicios.addChoice(objeto);
 
 
 
 
                 }
-
-                kulturas.setKulturaCode(jsonObject.getString("kulturaCode"));
-                kulturas.setSize(jsonObject.getInt("total"));
-
+                ejercicios.setLessonCode(jsonObject.getString("lessonCode"));
+                ejercicios.setSize(jsonObject.getInt("total"));
 
 
 
 
 
-                return kulturas;
+
+                return ejercicios;
             }
 
             @Override
@@ -72,12 +78,13 @@ public class RecordActivity extends AppCompatActivity {
                 /*img.setImageBitmap(result);
                 img.setVisibility(View.VISIBLE);
                 Toast.makeText(context,"Aqui he llegado",Toast.LENGTH_SHORT).show();*/
-                lkultura=result.lkultura;
+                lExercise=result.lExercise;
                 size=result.getSize();
+                Toast.makeText(context,"Exito",Toast.LENGTH_SHORT).show();
 
 
 
-                ChangeView(text,title);
+                //ChangeView(text,title);
 
                 //img.setVisibility(View.VISIBLE);
 
