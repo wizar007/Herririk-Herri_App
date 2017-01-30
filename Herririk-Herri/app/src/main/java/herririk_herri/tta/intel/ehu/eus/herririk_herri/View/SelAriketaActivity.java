057a1995a9e43f4086975bac20b05c82f29dd96f
Mikel De Prado <mikeldeprado@hotmail.com>
2017-01-30 18:51:02 +0100

    Arreglo bugfix onRestart() en activity selariketa

--- Herririk-Herri/app/src/main/java/herririk_herri/tta/intel/ehu/eus/herririk_herri/View/SelAriketaActivity.java
+++ Herririk-Herri/app/src/main/java/herririk_herri/tta/intel/ehu/eus/herririk_herri/View/SelAriketaActivity.java
@@ -39,6 +39,35 @@ public class SelAriketaActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void  onRestart()
+    {
+        super.onRestart();
+        setContentView(R.layout.activity_sel_ariketa);
+        SharedPreferences prefs = getSharedPreferences("user", MODE_WORLD_READABLE);
+        int ariketa2=prefs.getInt("unlockariketa2",0);
+        int ariketa3=prefs.getInt("unlockariketa3",0);
+        Button mButton=(Button)findViewById(R.id.selArik_button_img);
+        if(ariketa2==1)
+        {
+            mButton.setCompoundDrawables(null,null,null,null);
+        }
+        else
+        {
+            mButton.setOnClickListener(null);
+        }
+        mButton=(Button)findViewById(R.id.selArik_button_record);
+        if(ariketa3==1)
+        {
+            mButton.setCompoundDrawables(null,null,null,null);
+        }
+        else
+        {
+            mButton.setOnClickListener(null);
+        }
+
+    }
+
     public void selAriketa(View view)
     {
 
